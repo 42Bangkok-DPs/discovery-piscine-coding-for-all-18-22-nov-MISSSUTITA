71e1acaedff019002f50afe544adb67f5ac37af6
MISSSUTITA <wimonrat061118@gmail.com>
2024-11-20 10:48:33 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,5 @@
+first_name = ('Nathakorn')
+last_name = ('Akeakarabundit')
+
+print (first_name)
+print (last_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,5 @@
+your_age = int(input("how old are you?"))
+
+my_age = your_age + 42
+
+print(my_age)
