f993de8704bc717adecfe265d868f588e89dea61
MISSSUTITA <wimonrat061118@gmail.com>
2024-11-20 10:49:04 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+number = float(input("enter your number :"))
+
+if number == 0:
+    print("This number is equal to zero.")
+else :
+    print("This number is different from zero.")
---
+++ cell02/ex02/password.py.py
@@ -0,0 +1,7 @@
+password = "Python is awesome"
+entered_password = input("Password is: ")
+
+if entered_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
